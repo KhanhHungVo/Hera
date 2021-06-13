f9a567e43d6c2c570758dc05f4b076b21c2578ef
KhanhHungVo <khanhhungvo001@gmail.com>
2021-06-13 10:06:43 +0700

    add order type

---
+++ Hera.Common/Enums/OrderType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hera.Common.Enums
+{
+    public enum OrderType
+    {
+        Unkwown = 0,
+        WithDrawDeposit = 1,
+        Crypto = 2,
+        Stock = 3,
+        Forex = 4
+    }
+}
--- Hera.Data/Entities/OrderEntity.cs
+++ Hera.Data/Entities/OrderEntity.cs
@@ -9,19 +9,34 @@ namespace Hera.Data.Entities
 {
     public class OrderEntity : EntityBaseTypeId<int>
     {
-        public string UserId { get; set; }
+        //public string UserId { get; set; }
         public DateTime OrderDate { get; set; }
         public string Name { get; set; }
         public string Symbol { get; set; }
         public int Volume { get; set; }
         public string Reason { get; set; }
+        /// <summary>
+        /// Tổng giá trị đầu tư
+        /// </summary>
         public double InvestmentValue { get; set; }
+
+        /// <summary>
+        /// Giá mua vào/ giá vốn trên 1 đơn vị
+        /// </summary>
         public double OrderPrice { get; set; }
+
+        /// <summary>
+        /// Giá hiện tại trên 1 đơn vị
+        /// </summary>
         public double CurrentPrice { get; set; }
+        /// <summary>
+        /// Tổng giá trị hiện tại theo giá thị trường
+        /// </summary>
         public double MarketValue { get; set; }
         public double GainLossPercentage { get; set; }
         public double GainLoss { get; set; }
         public Boolean Done { get; set; }
+        public int Type { get; set; }
 
     }
 
